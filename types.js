7e5911b00bf02768c85a74e83dec183bc00a8f80
Matthew Vita <matthewvita@live.com>
2014-07-14 18:40:54 -0400

    logic added to options setup

--- types.js
+++ types.js
@@ -4,10 +4,10 @@
     var t = function(value, constructor, options) {
         // Evaluate options
         options = options || {};
-        options.enabled = options.enabled || true;
-        options.errors = options.errors || false;
-        options.silent = options.silent || false;
-        options.required = options.required || false;
+        options.enabled = options.enabled === false? false: true;
+        options.errors = options.errors === true? true: false;
+        options.silent = options.silent === true? true: false;
+        options.required = options.required === true? true: false;
 
         // Throw an error, and use `console.error` if possible.
         var error = function(message) {
